45d53b03d5f8d2a23c0e227dcb9590a5cedba94d
NeroSannn <genshinislife25@gmail.com>
2025-07-11 15:18:37 +0800

    Added landing page, main homepage, updated images and styles

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index'); // landing page
+});
+
+app.get('/home', (req, res) => {
+  res.render('home'); // main website
 });
 
 app.listen(PORT, () => {
